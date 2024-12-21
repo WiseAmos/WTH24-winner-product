dc6afe77badbc090a1f0c2c4644e763b67c63743
dallaschan1 <s10258596@connect.np.edu.sg>
2024-12-21 20:51:19 +0800

    sda sas

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/static')));
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('/announcement', (req, res) => {
     res.sendFile(path.join(__dirname, '/static/post/post.html'));
 });
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/html/signup.html'));
+});
+app.get('/signup/signup-volunteer', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/html/signup-volunteer.html'));
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
